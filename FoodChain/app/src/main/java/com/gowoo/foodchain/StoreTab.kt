6eae44a5326af7a5ca909e3eb7a1c187491f21e9
choigone <goun4439@gmail.com>
2018-07-03 23:24:57 +0900

    for 승맹

---
+++ FoodChain/app/src/main/java/com/gowoo/foodchain/StoreTab.kt
@@ -0,0 +1,14 @@
+package com.gowoo.foodchain
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class StoreTab : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        //val v = inflater.inflate(R.layout.activity_)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
